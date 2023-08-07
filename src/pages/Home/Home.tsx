f60be513e825ae9251838629591724a150fa854b
Matheus Castro <goca1377@gmail.com>
2023-08-07 10:53:46 -0300

    feat: add refresh session

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from 'react'
+import { api } from '../../services/api'
+
 export function Home() {
+  useEffect(() => {
+    api
+      .get('/books')
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error))
+  }, [])
+
   return <h1>Home Page</h1>
 }

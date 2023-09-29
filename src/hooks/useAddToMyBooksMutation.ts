96c57b5cf789ca2613e1e59a8621056fe1cbf7fe
Matheus Castro <goca1377@gmail.com>
2023-09-29 14:08:02 -0300

    feat: add optimistic update to addToMyBooks

--- src/hooks/useAddToMyBooksMutation.ts
+++ src/hooks/useAddToMyBooksMutation.ts
@@ -1,6 +1,9 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
 import { BookState } from '../models/BookState'
 import { MyBook } from '../models/MyBook'
+import { Book } from '../models/Book'
+
 import { api } from '../services/api'
 
 interface AddToMyBooksMutationArgs {
@@ -21,8 +24,38 @@ export async function addToMyBooks({
 }
 
 export function useAddToMyBooksMutation() {
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: async (data: AddToMyBooksMutationArgs) =>
-      await addToMyBooks(data)
+      await addToMyBooks(data),
+    onMutate: async (data) => {
+      const bookDetailKey = ['book-details', data.bookId]
+
+      await queryClient.cancelQueries({
+        queryKey: bookDetailKey
+      })
+
+      const previousBookDetail = queryClient.getQueryData(bookDetailKey)
+
+      queryClient.setQueryData<Book>(bookDetailKey, (oldData) => {
+        return oldData
+          ? {
+              ...oldData,
+              bookState: data.bookState
+            }
+          : oldData
+      })
+
+      return { previousBookDetail, data }
+    },
+    onError: (error, data, context) => {
+      const bookDetailKey = ['book-details', context?.data.bookId]
+
+      queryClient.setQueryData(bookDetailKey, context?.previousBookDetail)
+    },
+    onSettled: (data) => {
+      const bookDetailKey = ['book-details', data?.bookId]
+      queryClient.invalidateQueries({ queryKey: bookDetailKey })
+    }
   })
 }

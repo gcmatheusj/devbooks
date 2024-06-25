a35fc86de7ebcbd9f32a9d5a7c6fa771cda4db15
Matheus Castro <goca1377@gmail.com>
2024-06-25 20:40:05 -0300

    feat: add change password

--- src/components/Security/Security.tsx
+++ src/components/Security/Security.tsx
@@ -1,18 +1,72 @@
+import { useState } from 'react'
+
 import { Button } from '../Button'
 import { Input } from '../Input'
+import { AlertBanner } from '../AlertBanner'
+
+import { useError } from '../../hooks/useError'
+import { useUpdatePasswordMutation } from '../../hooks/useUpdatePasswordMutation'
 
 import { Container, UpdatePasswordContainer } from './Security.styles'
 
 export function Security() {
+  const [currentPassword, setCurrentPassword] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const { mutateAsync, isLoading } = useUpdatePasswordMutation()
+  const { error, handleError, clearError } = useError()
+  const [success, setSuccess] = useState(false)
+
+  const handleUpdatePassword = async () => {
+    try {
+      clearError()
+      setSuccess(false)
+
+      await mutateAsync({
+        newPassword,
+        currentPassword
+      })
+
+      setCurrentPassword('')
+      setNewPassword('')
+      setSuccess(true)
+    } catch (error) {
+      handleError(error)
+    }
+  }
+
   return (
     <Container>
       <h2>Segurança</h2>
 
       <UpdatePasswordContainer>
-        <Input label="Senha Atual" type="password" />
-        <Input label="Nova Senha" type="password" />
-        <Button>Atualizar Senha</Button>
+        <Input
+          label="Senha Atual"
+          type="password"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+        />
+        <Input
+          label="Nova Senha"
+          type="password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <Button
+          disabled={!newPassword || !currentPassword || isLoading}
+          onClick={handleUpdatePassword}
+        >
+          {isLoading ? 'Atualizando...' : 'Atualizar Senha'}
+        </Button>
       </UpdatePasswordContainer>
+
+      {success && (
+        <AlertBanner
+          variant="success"
+          message="Senha atualizada com sucesso!"
+        />
+      )}
+
+      {error && <AlertBanner variant="error" message={error} />}
     </Container>
   )
 }
